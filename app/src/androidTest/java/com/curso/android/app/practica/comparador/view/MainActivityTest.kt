6f737da0393f36929ad87e53a4adb670523e31d2
Federico Francesconi <108153799+federicoFrancesconi@users.noreply.github.com>
2023-09-03 15:43:11 -0300

    Tests incrementales y unit

--- app/src/androidTest/java/com/curso/android/app/practica/comparador/view/MainActivityTest.kt
+++ app/src/androidTest/java/com/curso/android/app/practica/comparador/view/MainActivityTest.kt
@@ -1,5 +1,66 @@
 package com.curso.android.app.practica.comparador.view
 
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.platform.app.InstrumentationRegistry
+import com.curso.android.app.practica.comparador.R
+
+//Importamos dependencias para los tests
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.After
+import org.junit.Before
+
+@RunWith(AndroidJUnit4::class)
 class MainActivityTest {
 
+    @get:Rule
+    var rule: ActivityScenarioRule<MainActivity> = ActivityScenarioRule(MainActivity::class.java)
+
+    @Before
+    fun setUp() {
+    }
+
+    @After
+    fun tearDown() {
+    }
+
+    @Test
+    fun mainActivity_compareStringsIguales() {
+        // Ingresa el texto en los editText
+        Espresso.onView(ViewMatchers.withId(R.id.editText1))
+            .perform(ViewActions.typeText("Hello"))
+        Espresso.onView(ViewMatchers.withId(R.id.editText2))
+            .perform(ViewActions.typeText("Hello"))
+
+        // Hace click en el botón comparar
+        Espresso.onView(ViewMatchers.withId(R.id.buttonComparar))
+            .perform(ViewActions.click())
+
+        // Revisa que el textView indique que las cadenas ingresadas coinciden
+        Espresso.onView(ViewMatchers.withId(R.id.textView))
+            .check(ViewAssertions.matches(ViewMatchers.withText("Las cadenas ingresadas coinciden")))
+    }
+
+    @Test
+    fun mainActivity_compareStringsDiferentes() {
+        // Ingresa el texto en los editText
+        Espresso.onView(ViewMatchers.withId(R.id.editText1))
+            .perform(ViewActions.typeText("Hello"))
+        Espresso.onView(ViewMatchers.withId(R.id.editText2))
+            .perform(ViewActions.typeText("Hola"))
+
+        // Hace click en el botón comparar
+        Espresso.onView(ViewMatchers.withId(R.id.buttonComparar))
+            .perform(ViewActions.click())
+
+        // Revisa que el textView indique que las cadenas ingresadas son diferentes
+        Espresso.onView(ViewMatchers.withId(R.id.textView))
+            .check(ViewAssertions.matches(ViewMatchers.withText("Las cadenas ingresadas son diferentes")))
+    }
 }
--- app/src/test/java/com/curso/android/app/practica/comparador/MainViewModelUnitTest.kt
+++ app/src/test/java/com/curso/android/app/practica/comparador/MainViewModelUnitTest.kt
@@ -1,5 +1,70 @@
 package com.curso.android.app.practica.comparador
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.curso.android.app.practica.comparador.view.MainViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.test.*
+import org.junit.Assert.*
+
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.After
+
+
+@OptIn(kotlinx.coroutines.ExperimentalCoroutinesApi::class)
 class MainViewModelUnitTest {
 
+    private lateinit var viewModel: MainViewModel
+
+    @get:Rule
+    //Especifica que LiveData se actualiza automáticamente
+    val instantTaskRule = InstantTaskExecutorRule()
+    private val dispatcher = StandardTestDispatcher()
+
+    //Antes de los tests seteamos el dispatcher
+    @Before
+    fun setup() {
+        Dispatchers.setMain(dispatcher)
+        viewModel = MainViewModel()
+    }
+
+    //Despues de los tests reseteamos el dispatcher
+    @After
+    fun tearDown() {
+        Dispatchers.resetMain()
+    }
+
+    @Test
+    fun mainViewModel_CompareStrings_Same() = runTest {
+        //Elegimos dos strings iguales
+        val input1 = "Hello"
+        val input2 = "Hello"
+
+        //Ejecutamos la función de forma asincronica
+        launch {
+            viewModel.compararStrings(input1, input2)
+        }
+        advanceUntilIdle()
+
+        val resultado = viewModel.resultadoComparacion.value
+        assertEquals("Las cadenas ingresadas coinciden", resultado)
+    }
+
+    @Test
+    fun mainViewModel_CompareStrings_Different() = runTest {
+        //Elegimos dos strings que sean diferentes
+        val input1 = "Hello"
+        val input2 = "Hola"
+
+        //Ejecutamos la función de forma asincronica
+        launch {
+            viewModel.compararStrings(input1, input2)
+        }
+        advanceUntilIdle()
+
+        val resultado = viewModel.resultadoComparacion.value
+        assertEquals("Las cadenas ingresadas son diferentes", resultado)
+    }
 }
